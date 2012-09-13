e757b49871f2b39074a3d84cfe0d11f5043af2c4
Sarkis Karayan <skarayan@gmail.com>
2012-09-13 17:59:34 -0400

    Added requires

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -1,4 +1,13 @@
-require "core_extensions/version"
+require 'core_extensions/version'
+
+require 'core_extensions/object'
+require 'core_extensions/nil'
+require 'core_extensions/string'
+require 'core_extensions/symbol'
+require 'core_extensions/integer'
+require 'core_extensions/float'
+require 'core_extensions/array'
+require 'core_extensions/hash'
 
 module CoreExtensions
   # Your code goes here...
