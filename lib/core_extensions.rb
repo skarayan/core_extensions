1e0342e7c5d1bce9cc88f148b916cb20a1f3b639
Sarkis Karayan <skarayan@gmail.com>
2012-09-13 18:05:00 -0400

    Added activesupport requires

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -1,5 +1,13 @@
 require 'core_extensions/version'
 
+require 'active_support/inflector'
+require 'active_support/core_ext/string'
+require 'active_support/core_ext/hash/slice'
+require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/core_ext/array/grouping'
+require 'active_support/time'
+require 'active_support/time_with_zone'
+
 require 'core_extensions/object'
 require 'core_extensions/nil'
 require 'core_extensions/string'
